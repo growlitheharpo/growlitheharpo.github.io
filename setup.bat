40d2e1b56d9ab26fb8aa00c43393757b090e0202
James Keats <growlitheharpo@gmail.com>
2020-02-17 10:15:18 -0500

    Specify a version of purgecss

--- setup.bat
+++ setup.bat
@@ -3,7 +3,7 @@ echo "NOTE: You must have node and npm on your path for all utilities to work!"
 pushd %~dp0
 cd _tailwind
 call npm install tailwindcss
-call npm install purgecss
+call npm install purgecss@^1.4.0
 call npm install clean-css
 call npm install -g clean-css-cli
 popd
